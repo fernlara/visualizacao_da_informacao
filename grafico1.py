84d499fd51bda4055faf1dfe2aa3e3bb887db2e1
Fernando Lara <fernlara@gmail.com>
2024-05-29 14:10:43 -0300

    Versao definitiva

--- grafico1.py
+++ grafico1.py
@@ -1,5 +1,7 @@
 import plotly.express as px
 import pandas as pd
+from matplotlib import pyplot as plt
+from plotly import graph_objects as go
 
 # Importando arquivo CSV que será usado
 dataset = pd.read_csv('https://github.com/fernlara/visualizacao_da_informacao/raw/master/src/dataset.csv')
@@ -17,3 +19,46 @@ fig = px.bar(x=x_data, y=y_data, orientation='h', labels={'y': 'Causa', 'x': 'Mo
             title='Principais causas de morte no mundo em 2017 (Fonte: OMS)', text=x_data, range_y=[0, 10])
 # Renderizando gráfico
 fig.show()
+
+# Importando dataset base
+file_url = 'https://github.com/fernlara/visualizacao_da_informacao/raw/master/src/dataset.csv'
+dataset = pd.read_csv(file_url)
+dataset = pd.DataFrame(dataset)
+# Tratando dataset e selecionando os dados que serão usados
+topic = 'Deaths - Road injuries - Sex: Both - Age: All Ages (Number)'
+deaths_df = dataset.loc[dataset['Year'] == 2017, ['Entity','Year', topic]]
+numbers = list(pd.Series(deaths_df[topic]))
+entities = list(pd.Series(deaths_df.Entity))
+# Removendo a entidade Mundo da lista
+entities.pop(-4)
+numbers.pop(-4)
+# Criando imagem
+fig = go.Figure(data=go.Choropleth(
+    locations=entities,
+    z = numbers,
+    locationmode = 'country names',
+    colorscale = ('yellow','orange','red'),
+    colorbar_title = "Mortes",
+    )
+)
+fig.update_layout(title_text='Mortes em acidentes de transito no mundo em 2017, segundo OMS ((Organização Mundial da Saúde)')
+# Renderizando
+fig.show()
+
+#Importando dataset base
+file_url = 'https://github.com/fernlara/visualizacao_da_informacao/raw/master/src/dataset.csv'
+dataset = pd.read_csv(file_url)
+dataset = pd.DataFrame(dataset)
+#Tratando dataset e selecionando os dados que serão usados
+topic = 'Deaths - Road injuries - Sex: Both - Age: All Ages (Number)'
+deaths_df = dataset.loc[dataset['Entity'] == 'World', ['Entity','Year', topic]]
+numbers = list(pd.Series(deaths_df[topic]))
+years = list(pd.Series(deaths_df.Year))
+#Criando uma list para tornar mais simples e visualmente amigável o plot
+deaths_df = [years, numbers]
+# Criando o gráfico
+plt.plot(years, numbers)
+plt.title('Mortes no trânsito de 1990 a 2017, segundo a OMS (Organização Mundial da Saúde)')
+plt.ylabel('Mortes (em milhões)')
+# Renderizando
+plt.show()
